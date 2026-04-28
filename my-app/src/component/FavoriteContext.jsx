d8958758bd1a815cc5d7cea9712d6c022b03b089
thanh676 <thanh12345rrr@gmail.com>
2026-04-29 11:01:39 +1200

    Create FavoriteContext.jsx
    create favorite context to share state through page

---
+++ my-app/src/component/FavoriteContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useContext, useState } from "react";
+
+const FavoriteContext = createContext();
+
+export const FavoriteProvider = ({ children }) => {
+  const [favoriteItems, setFavoriteItems] = useState([]);
+
+  const handleAddToFavorite = (product) => {
+    setFavoriteItems((prev) => {
+      const exists = prev.find((item) => item.id === product.id);
+
+      if (exists) {
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+      }
+
+      return [...prev, { ...product, quantity: 1 }];
+    });
+  };
+
+  const handleRemoveFromFavorite = (product) => {
+    setFavoriteItems((prev) => {
+      const exists = prev.find((item) => item.id === product.id);
+
+      if (exists) {
+        // nếu quantity = 1 thì xóa luôn khỏi cart
+        if (exists.quantity === 1) {
+          return prev.filter((item) => item.id !== product.id);
+        }
+        // nếu quantity > 1 thì -1
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item,
+        );
+      }
+      return prev; // không tìm thấy thì giữ nguyên
+    });
+  };
+
+  const totalFavorites = favoriteItems.reduce(
+    (sum, item) => sum + item.quantity,
+    0,
+  );
+
+  return (
+    <FavoriteContext.Provider
+      value={{
+        favoriteItems,
+        handleAddToFavorite,
+        handleRemoveFromFavorite,
+        totalFavorites,
+      }}
+    >
+      {children}
+    </FavoriteContext.Provider>
+  );
+};
+
+export const useFavorite = () => useContext(FavoriteContext);
