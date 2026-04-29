9953be8e97806d827f5339bf7814384eeb196030
WarDenToC <amrithkong20011@gmail.com>
2026-04-29 17:48:14 +1200

    Django view and default router

--- HappyHH/urls.py
+++ HappyHH/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from products.views import ProductViewSet, CategoryViewSet
+from orders.views import CartViewSet, Order_ProductViewSet
+
+router = DefaultRouter()
+router.register('products', ProductViewSet)
+router.register('categories', CategoryViewSet)
+router.register('cart', CartViewSet)
+router.register('order_product', Order_ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
---
+++ users/serializers.py
@@ -0,0 +1,15 @@
+# users/serializers.py
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
