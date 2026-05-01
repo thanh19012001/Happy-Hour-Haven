f466e21f2fb54829a25206f589717cebe7735297
thanh676 <thanh12345rrr@gmail.com>
2026-05-01 15:13:14 +1200

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,4 +4,9 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.js",
+  },
 });
