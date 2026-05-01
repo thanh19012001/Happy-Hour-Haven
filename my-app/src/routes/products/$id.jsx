f9ba1e7ff819b4f406f5461d389f447090565929
thanh676 <thanh12345rrr@gmail.com>
2026-05-01 12:27:05 +1200

    delete and make a new file to make sure the route is working

--- my-app/src/routes/products/$id.jsx
+++ my-app/src/routes/products/$id.jsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import ProductPage from "../../component/ProductPage";
 
-export const Route = createFileRoute("/product/$id")({
+export const Route = createFileRoute("/products/$id")({
   component: RouteComponent,
 });
 
