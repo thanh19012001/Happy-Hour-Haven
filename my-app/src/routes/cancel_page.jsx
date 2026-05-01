939921df72856b302fbadfbbd3ba9bd92f3a3ced
thanh676 <thanh12345rrr@gmail.com>
2026-05-01 13:22:55 +1200

    Update cancel_page.jsx
    remove not relative impỏtt

--- my-app/src/routes/cancel_page.jsx
+++ my-app/src/routes/cancel_page.jsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import CartPage from "../component/CartPage";
 import Cancel from "../component/Cancel";
 
 export const Route = createFileRoute("/cancel_page")({
